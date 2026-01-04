ec0a700904c11cf64e43f48bef43e02b2b704df3
Tejas Durge <tejas.y.durge@gmail.com>
2026-01-04 14:17:49 +0530

    implemented bfs of graph using deque

--- 36_graph_traversal_bfs_and_dfs/bfs.py
+++ 36_graph_traversal_bfs_and_dfs/bfs.py
@@ -26,12 +26,31 @@ print(value)
 print(visited)
 """
 
-#bfs
+#bfs with array as stack
+print("--bfs using array--")
 visited=set()
 visited.add('e')
 stack_array=['e']
 while stack_array:
     currentVertex=stack_array.pop(0)
+    print(currentVertex)
+    for values in customDict[currentVertex]:
+        if values not in stack_array and values not in visited:
+            visited.add(values)
+            stack_array.append(values)
+
+
+# bfs with deque 
+# to pop out first element in 0(1) complexity 
+# as removing 0th index from array takes O(N) complexity
+print("--bfs using deque--")
+from collections import deque
+
+visited=set()
+visited.add('e')
+stack_array=deque(['e'])
+while stack_array:
+    currentVertex=stack_array.popleft()
     print(currentVertex)
     for values in customDict[currentVertex]:
         if values not in stack_array and values not in visited:
